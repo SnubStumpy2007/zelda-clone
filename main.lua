b2f81be39fdebe827d14c7429f8923b0e2ecd7bc
SnubStumpy2007 <joshua192007@gmail.com>
2024-11-19 18:48:49 -0500

    added sample player movmement

--- main.lua
+++ main.lua
@@ -1,9 +1,19 @@
 function love.load()
-    
+  player = require('modules.player')
 end
 
 function love.update(dt)
+  if love.keyboard.isDown('w') then
+    player.y = player.y - player.speed
+    elseif love.keyboard.isDown('a') then
+      player.x = player.x - player.speed
+    elseif love.keyboard.isDown('s') then
+      player.y = player.y + player.speed
+    elseif love.keyboard.isDown('d') then
+      player.x = player.x + player.speed
+    end
 end
 
 function love.draw()
+  love.graphics.draw(player.sprite, player.x, player.y)
 end
---
+++ modules/player.lua
@@ -0,0 +1,7 @@
+player = {}
+player.x = 400
+player.y = 300
+player.speed = 5
+player.sprite = love.graphics.newImage('/sprites/parrot.png')
+
+return player
