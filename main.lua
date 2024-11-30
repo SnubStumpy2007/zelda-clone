ec61b48b4fdb6a618a48de75cc60d6006281ee90
SnubStumpy2007 <joshua192007@gmail.com>
2024-11-29 20:07:38 -0500

    movement animations for character are done

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 
 function love.load()
+  -- set window size
+  love.window.setMode(1024, 640)
+
     -- libraries
  anim8 = require 'libraries/anim8'
   -- modules
@@ -10,20 +13,27 @@ function love.load()
 end
 
 function love.update(dt)
+  -- movement
   if love.keyboard.isDown('w') then
     player.y = player.y - player.speed
+    player.anim = player.animations.up
+   -- player.scaleX = -2
     elseif love.keyboard.isDown('a') then
       player.x = player.x - player.speed
+      player.anim = player.animations.left
     elseif love.keyboard.isDown('s') then
       player.y = player.y + player.speed
+      player.anim = player.animations.down
     elseif love.keyboard.isDown('d') then
       player.x = player.x + player.speed
+      player.anim = player.animations.right
+      -- player.scaleX = 2
     end
 
     player.anim:update(dt)
 end
 
 function love.draw()
---  love.graphics.draw(player.spriteSheet, player.x, player.y)
- player.anim:draw(player.spriteSheet, player.x, player.y)
+
+ player.anim:draw(player.spriteSheet, player.x , player.y, 0, 2.5, 2.5)
 end
--- modules/player.lua
+++ modules/player.lua
@@ -5,13 +5,13 @@ player.speed = 5
 player.spriteSheet = love.graphics.newImage('/sprites/player.png')
 -- https://opengameart.org/content/zoria-tileset
 player.grid = anim8.newGrid(16, 16, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
-
+--player.scaleX = 2
 
 -- animations
 player.animations = {}
 player.animations.down = anim8.newAnimation(player.grid('1-1', 1), 0.2)
 player.animations.right = anim8.newAnimation(player.grid('3-3', 1), 0.2)
-player.animations.left = anim8.newAnimation(player.grid('3-3', 1), 0.2)
+player.animations.left = anim8.newAnimation(player.grid('3-3', 1), 0.2):flipH()
 player.animations.up = anim8.newAnimation(player.grid('2-2', 1), 0.2)
 
 player.anim = player.animations.down
